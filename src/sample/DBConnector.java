983775ad9ce7c925a1990d61467a51b95070d92c
Jamie Stimpson <44094740+Jamnic98@users.noreply.github.com>
2021-02-23 10:22:28 +0000

    Update DBConnector.java

--- src/sample/DBConnector.java
+++ src/sample/DBConnector.java
@@ -5,20 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-
-<<<<<<< HEAD
     private static final Logger logger = Logger.getLogger(DBConnector.class.getName());
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_CONNECTION = "jdbc:mysql://localhost/userdb?serverTimezone=UTC";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "";
-=======
-    // C:\Users\Jamie\Documents\Programming\Reading Log
->>>>>>> 972838c (initial commit)
 
     public static Connection getDBConnection() {
         Connection connection;
-
         try {
             // db parameters
             String sqlDBLocation = "src\\sample\\my_books.db";
@@ -26,7 +20,6 @@ public class DBConnector {
             // create a connection to the database
             connection = DriverManager.getConnection(url);
             return connection;
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
